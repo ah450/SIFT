ecdc7df96628e8bbf74b675ad146f2ddabd26dad
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-11-20 14:42:10 +0200

    Adds support for Clang and MSVC compilers

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,17 +3,29 @@
 #include <ctype.h>
 #include <cmath>
 
+
+
+
 namespace sift {
     constexpr auto IMAGE_DATA_TYPE = CV_8U;
-    typedef uint8_t image_t ;
+    typedef uint8_t image_t;
 
     constexpr auto GAUSSIAN_PYR_KERNEL_SIZE = 3;
     // sigma of the gaussian blur kernel
-    constexpr auto GAUSSIAN_PYR_SIGMA0      = std::sqrt(2);
+    #ifdef  WE_HAVE_CONST_MATH
+        constexpr auto GAUSSIAN_PYR_SIGMA0      = std::sqrt(2);
+    #else
+        const auto GAUSSIAN_PYR_SIGMA0      = std::sqrt(2);
+    #endif
     // size of each octave is s+3
     constexpr auto GAUSSIAN_PYR_S           = 2;
     // factor between sigma of each blurred image
-    constexpr auto GAUSSIAN_PYR_K           = std::pow(2.0, 1.0/GAUSSIAN_PYR_S);
+    #ifdef  WE_HAVE_CONST_MATH
+        constexpr auto GAUSSIAN_PYR_K           = std::pow(2.0, 1.0/GAUSSIAN_PYR_S);    
+    #else
+        const auto GAUSSIAN_PYR_K           = std::pow(2.0, 1.0/GAUSSIAN_PYR_S);  
+    #endif
+
     // size of each octave (number of blurred images)
     constexpr auto GAUSSIAN_PYR_OCTAVE_SIZE = GAUSSIAN_PYR_S + 3;
 }
