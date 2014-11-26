685d4e32bef313ea2e8ecf13a1c2b5104f031f9b
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-11-26 09:49:53 +0200

    Moves Neighbourhood to internal.hpp

--- src/orientations.cpp
+++ src/orientations.cpp
@@ -62,50 +62,7 @@ double delta_x(const Mat &image, int x, int y) {
 }
 
 
-/**
- * @brief Represents Rectangular area.
- * @detail Either a rectangle whose size is a parameter or one for 
- * a gaussian kernel based on scale value with a minimum of 5x5.
- */
-struct Neighbourhood {
-    int kernel_size, row_start, row_end, col_start, col_end;
 
-    /**
-     * @brief Constructs a Neighbourhood for a gaussian kernel
-     * @param num_rows Total number of rows in parent region
-     * @param num_columns Total number of columns in parent region
-     * @param row center row value
-     * @param col center col value
-     * @param scale gauss sigma
-     * @param min minimum kernel size, defaults to 5.
-     */
-    Neighbourhood(int num_rows, int num_columns, int row, int col,
-                  double scale, double min = 5.0) {
-        kernel_size = std::max(3 * scale, min);
-        if (kernel_size % 2  == 0) {
-            kernel_size += 1;
-        }
-        int kernel_half = kernel_size / 2;
-        calc_rect(num_rows, num_columns, row, col, kernel_half);
-    }
-
-    /**
-     * @brief Calculates a size * size rectangle  around row, col.
-     */
-    Neighbourhood(int num_rows, int num_columns, int row, int col, int size=16) {
-        calc_rect(num_rows, num_columns, row, col, size/2);
-    }
-
-private:
-    void calc_rect(int num_rows, int num_columns, int row, int col, int half_size){
-        row_start = row > half_size ? row - half_size : 0;
-        row_end = row + half_size < num_rows - 1 ? row + half_size + 1 : num_rows;
-        col_start = col > half_size ? col - half_size : 0;
-        col_end = col + half_size < num_columns - 1 ? col + half_size + 1 :
-                  num_columns;
-    }
-
-};
 
 
 
