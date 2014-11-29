7f96a48190ecf3d80c17f8d17dbe9f992e18a282
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-11-29 05:53:52 +0200

    Fixes minor issue with sift::downSample

--- src/utils.cpp
+++ src/utils.cpp
@@ -11,13 +11,11 @@
 cv::Mat sift::downSample(Mat &image) {
     Mat out(image.rows/2, image.cols /2, IMAGE_DATA_TYPE);
 
-    for (int row = 0; row < image.rows; row+=2 ) {
-        if (row % 2 == 0) {
-            for (int column = 0; column < image.cols; column+=2) {
-                if (column % 2 == 0)
-                    out.at<sift::image_t>(row/2, column/2) = image.at<sift::image_t>(row, column);
+    for (int row = 0; row < out.rows; row++ ) {
+            for (int column = 0; column < out.cols; column++) {
+                    out.at<sift::image_t>(row, column) = image.at<sift::image_t>(row * 2,
+                            column * 2);
             }
-        }
     }
 
     return out;
